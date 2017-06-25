1b847e001f4f431ca795a9c0cbd0245072fbb9f5
FgAdmin <FgAdmin@DESKTOP-OL6BVDA>
2017-06-25 17:45:03 +0800

    ModelBind

---
+++ WebAPI/WebAPIStudy/MyWebApiModelController/DemoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.ModelBinding;
+
+namespace MyWebApiModelController
+{
+    [ModelBinder]
+    [DataContract]
+    public class DemoModel
+    {
+        [DataMember]
+        public string UserName { get; set; }
+        [DataMember]
+        public string PassWord { get; set; }
+    }
+}
---
+++ WebAPI/WebAPIStudy/MyWebApiModelController/ModelBindController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MyWebApiModelController
+{
+    [RoutePrefix("api2/modelBind")]
+  public  class ModelBindController:ApiController
+    {
+
+        [AcceptVerbs("Get","Post")]
+       // [HttpGet]
+        [Route("setModelData/{UserName}/{PassWord}")]
+        public DemoModel SetModelData(DemoModel demoModel)
+        {
+            Console.WriteLine(demoModel.UserName);
+            Console.WriteLine(demoModel.PassWord);
+            return demoModel;
+
+        }
+    }
+}
