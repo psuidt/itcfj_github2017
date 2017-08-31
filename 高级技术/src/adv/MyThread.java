3690efd69ef2b793b8e09a80b4ccaf409c67dd90
MRJZ <MRJZ@DESKTOP-EI7SVA5>
2017-08-31 22:31:32 +0800

    dd

--- 高级技术/src/adv/MyThread.java
+++ 高级技术/src/adv/MyThread.java
@@ -1,9 +1,14 @@
 package adv;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyThread {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Map<Thread, String> Store=new HashMap<Thread, String>();
 		
 		for (int i = 0; i < 3; i++) {
 			new Thread(
@@ -12,12 +17,15 @@ public class MyThread {
 						@Override
 						public void run() {
 							// TODO Auto-generated method stub
-							
+							String tmp=String.valueOf(Math.random());
+							Store.put(Thread.currentThread(), tmp);
 							System.err.println(Thread.currentThread().getName());
 						}
 					}
 					).start();
 		}
+		
+		
 		
 
 	}
